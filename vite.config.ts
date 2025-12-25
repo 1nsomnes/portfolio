f11b55e65609c530e62426f978d7e02fd38d8b3e
1nsomnes <1nsomnes@users.noreply.github.com>
2025-12-25 00:57:36 -0800

    use hash router and make file refs work

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ }) => ({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: mode === 'production' ? '/portfolio/' : '/',
+  base: '/',
 }))
